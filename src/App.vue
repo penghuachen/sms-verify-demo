b254b70f73d5b3d8734a3d62c0ae717f57be5cbc
Penghua Chen <kids5346@gmail.com>
2022-04-06 17:54:17 +0800

    remove legacy code

--- src/App.vue
+++ src/App.vue
@@ -67,18 +67,6 @@ export default {
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 
 input[type="text"] {
   width: 50px;
